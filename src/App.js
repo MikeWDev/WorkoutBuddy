c5f3aeb55b26ff444fd4be310b6468266c9a35f0
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2024-03-21 10:04:20 +0100

    Added function to assign workouts with users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./Pages/Home";
 import Navbar from "./componets/navbar";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
           </Routes>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
           </Routes>
           <Routes>
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
